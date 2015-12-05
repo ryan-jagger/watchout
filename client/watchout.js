48d5c5e7dece2dfe466220fc94cffeb0ebc3c0bd
Ryan Jagger <ryanjagger@gmail.com>
2015-12-05 12:47:14 -0600

    Draggable player dot implemented;

--- client/watchout.js
+++ client/watchout.js
@@ -12,20 +12,16 @@ var gameStats = {
   bestScore: 0,
 };
 
-
 var axes = {
   x: d3.scale.linear().domain([0,100]).range([0,gameOptions.width]), 
   y: d3.scale.linear().domain([0,100]).range([0,gameOptions.height]) 
 };
 
-
 var gameBoard = d3.select('.container').append('svg:svg')
                   .attr('width', gameOptions.width)
                   .attr('height', gameOptions.height);
 
-
 var createEnemies = function () {
-
   return _(_.range(0, gameOptions.nEnemies))
             .map(function(value, i){ 
               return {
@@ -36,8 +32,41 @@ var createEnemies = function () {
                     color: 'MIDNIGHTBLUE'
                      };
             });
- 
 };
+
+
+var createPlayer = function(){
+
+  var player = {
+    height: 30,
+    width: 30,
+    fill: 'red',
+    x: axes.x(50),
+    y: axes.y(50),
+  };
+
+  var drag = d3.behavior.drag()
+               .on('dragstart', function(){ this.setAttribute('fill', 'green') })
+               .on('drag', function(){ this.setAttribute('x', d3.event.x - player.height/2); this.setAttribute('y', d3.event.y - player.height/2) })
+               .on('dragend', function(){ this.setAttribute('fill', 'red')  })
+
+
+
+  gameBoard.selectAll('.player')
+           .data([player])
+           .enter()
+           .append('svg:rect')
+           .attr('height', player.height)
+           .attr('width', player.width)
+           .attr('fill', player.fill)
+           .attr('x', player.x)
+           .attr('y', player.y) 
+           .call(drag)
+};
+
+
+
+
 
 
 var render = function(enemyData){
@@ -63,16 +92,49 @@ var render = function(enemyData){
 
 };
 
-
 setInterval( function(){ render(createEnemies()) } , 2000);
+createPlayer();
+
+
+/*
+
+TESTING STUFF
+
+
+gameBoard.selectAll('.test')
+          .data([1])
+          .enter()
+          .append('rect')
+          .attr('height',30)
+          .attr('width',30)
+          .attr('fill','white')
+          .attr('class','test')
+          .attr('x',50)
+          .attr('y',50)
+          .call(drag)
+
+gameBoard.selectAll('.test').  ...
+
+
+var drag = d3.behavior.drag()  
+             .on('dragstart', function(d) { this.setAttribute('fill', 'red'); })
+             .on('drag', function(d) {     this.setAttribute('x', d3.event.x)
+                                           this.setAttribute('y', d3.event.y); })
+             .on('dragend', function(e) { this.setAttribute('fill', 'black'); });
+
+
+var circle = box.selectAll('.test')  
+                .data([{ x:  , y: axes.y(Math.random()*100)}])
+                .enter()
+                .append('svg:circle')
+                .attr('class', 'draggableCircle')
+                .attr('cx', function(d) { return d.x; })
+                .attr('cy', function(d) { return d.y; })
+                .attr('r', function(d) { return d.r; })
+                
+                .style('fill', 'black');
 
 
 
 
-
-// d3 select our svg
-  // call data with our enemies
-  // call enter
-  // append circles/squares
-  // set attr for height/width/fill
-  // set attr for x,y and provide function that returns {}.x {}.y
+*/
